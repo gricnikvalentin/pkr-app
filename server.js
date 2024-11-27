1e83a44b53682899156de57ef235953167f5a73c
valentin.gricnik <valentin.gricnik@stroka.si>
2024-11-27 21:26:43 +0100

    bugfix3

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ app.use(express.static('public'));
 app.get('/admin' ,(req,res)=>{
     res.sendFile(path.join(__dirname, 'public/admin/index_admin.html'));
 })
+app.get('/' ,(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
 
 
 /// Path to JSON files
