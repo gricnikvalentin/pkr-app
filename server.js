ccbf290fa8b21f6bbf357a1406ea247038f95209
valentin.gricnik <valentin.gricnik@stroka.si>
2024-11-27 15:34:18 +0100

    lol

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const cors = require('cors');
-
+const http = require ('http');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+
 
 // Middleware
 app.use(bodyParser.json());
